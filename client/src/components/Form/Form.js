e886ee3244f189c0bd2e72932ef660f0e1ae7560
Alice <andreevaalisa42@gmail.com>
2021-09-10 17:19:48 +0300

    add posts reducer

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from 'react-file-base64';
 import { useDispatch } from 'react-redux';
-import { createPost } from "../../actions/posts";
+import { createPost, updatePost } from "../../actions/posts";
 import useStyles from './styles';
 
 const Form = ({ currentId, setCurrentId }) => {
@@ -19,7 +19,11 @@ const Form = ({ currentId, setCurrentId }) => {
   const handleSubmit = (e) => {
     e.preventDefault(); 
 
-    dispatch(createPost(postData));
+    if(currentId) {
+      dispatch(updatePost(currentId, postData));
+    } else {
+      dispatch(createPost(postData));
+    }
   }
 
   const clear = () => {
