8c06c9ecbe6cefecdef212ba01c9dca75f01c8dc
Alice <andreevaalisa42@gmail.com>
2021-09-12 13:38:46 +0300

    refactor app component

---
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import { Paper, Typography, CircularProgress, Divider } from '@material-ui/core/';
+import { useDispatch, useSelector } from 'react-redux';
+import moment from 'moment';
+import { useParams, useHistory } from 'react-router-dom';
+
+import { getPost, getPostsBySearch } from '../../actions/posts';
+import useStyles from './styles';
+
+const PostDetails = () => {
+  return (
+    <div>
+post de
+    </div>
+  )
+}
+
+export default PostDetails;
