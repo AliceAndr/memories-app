5e6abe52c53a6d73637e7716363ca07ee8130988
Alice <andreevaalisa42@gmail.com>
2021-09-12 15:10:00 +0300

    add pagination frontend

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,12 +5,32 @@ import PostMessage from '../models/postMessage.js';
 
 const router = express.Router();
 
-export const getPosts = async (req, res) => { 
+export const getPosts = async (req, res) => {
+    const { page } = req.query;
+    
     try {
-        const postMessages = await PostMessage.find();
-                
-        res.status(200).json(postMessages);
-    } catch (error) {
+        const LIMIT = 8;
+        const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
+    
+        const total = await PostMessage.countDocuments({});
+        const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
+
+        res.json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT)});
+    } catch (error) {    
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const getPostsBySearch = async (req, res) => {
+    const { searchQuery, tags } = req.query;
+
+    try {
+        const title = new RegExp(searchQuery, "i");
+
+        const posts = await PostMessage.find({ $or: [ { title }, { tags: { $in: tags.split(',') } } ]});
+
+        res.json({ data: posts });
+    } catch (error) {    
         res.status(404).json({ message: error.message });
     }
 }
@@ -27,20 +47,6 @@ export const getPost = async (req, res) => {
     }
 }
 
-export const getPostsBySearch = async (req, res) => {
-  const { searchQuery, tags } = req.query;
-
-  try {
-      const title = new RegExp(searchQuery, "i");
-
-      const posts = await PostMessage.find({ $or: [ { title }, { tags: { $in: tags.split(',') } } ]});
-
-      res.json({ data: posts });
-  } catch (error) {    
-      res.status(404).json({ message: error.message });
-  }
-}
-
 export const createPost = async (req, res) => {
     const post = req.body;
 
@@ -49,7 +55,7 @@ export const createPost = async (req, res) => {
     try {
         await newPostMessage.save();
 
-        res.status(201).json(newPostMessage );
+        res.status(201).json(newPostMessage);
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
@@ -57,11 +63,11 @@ export const createPost = async (req, res) => {
 
 export const updatePost = async (req, res) => {
     const { id } = req.params;
-    const { title, message, creator, likes, selectedFile, tags } = req.body;
+    const { title, message, creator, selectedFile, tags } = req.body;
     
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-    const updatedPost = { creator, likes, title, message, tags, selectedFile, _id: id };
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
 
     await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
 
@@ -99,6 +105,7 @@ export const likePost = async (req, res) => {
     const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
     res.status(200).json(updatedPost);
 }
+
 
 
 export default router;
