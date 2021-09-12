397f8b9b67039591fe39fbf0e1ee20c127cdfa15
Alice <andreevaalisa42@gmail.com>
2021-09-12 16:44:24 +0300

    add comment section structure frontend

---
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useRef } from 'react';
+import { Typography, TextField, Button } from '@material-ui/core/';
+import { useDispatch } from 'react-redux';
+
+import { commentPost } from '../../actions/posts';
+import useStyles from './styles';
+
+const CommentSection = () => {
+  return (
+    <div>
+      comment section
+    </div>
+  )
+}
+
+export default CommentSection;
